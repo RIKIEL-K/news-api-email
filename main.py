b62df5bdaec40e1f517b878a2d25cebe57c6b605
RIKIEL-K <rikkouambeng05@gmail.com>
2025-02-06 01:03:04 -0500

    add user interface with streamlit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from send_email import send_email
+import streamlit as st
 
 topic = "tesla"
 API_Key = "c8b8e95bcc724a73a6a07f384a6992a1"
@@ -7,10 +8,16 @@ url = f"https://newsapi.org/v2/everything?q={topic}&from=2025-01-06&sortBy=publi
 request = requests.get(url)
 content = request.json()
 
-msg="Subject: Today's news \n\n"
-for article in content["articles"][:20]:
-       if article["title"] is not None and article["description"] is not None:
-              msg += article["title"]+ "\n" + article["description"] + article["url"] +2*"\n"
+st.header("Get our newsletter")
 
-msg = msg.encode('utf-8')
-send_email(message=msg)
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    msg = "Subject: Today's news \n\n"
+    btn = st.form_submit_button()
+    if btn:
+           for article in content["articles"][:20]:
+                  if article["title"] is not None and article["description"] is not None:
+                         msg += article["title"] + "\n" + article["description"] + article["url"] + 2 * "\n"
+           msg = msg.encode('utf-8')
+           send_email(message=msg,receiver_email=user_email)
+           st.info("NewsLetter sended !")
--- send_email.py
+++ send_email.py
@@ -2,17 +2,16 @@ import smtplib
 import ssl
 
 
-def send_email(message):
+def send_email(message,receiver_email):
     host= "smtp.gmail.com"
     port = 465
 
     username= "elrik4448@gmail.com"
     password = "jqwa glad gfeg wzaz"
 
-    receiver = "elrik4448@gmail.com"
 
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host,port,context=context) as server:
         server.login(username,password)
-        server.sendmail(username,receiver,message)
+        server.sendmail(username,receiver_email,message)
